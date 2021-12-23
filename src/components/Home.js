8895ebb2d036b352dd70c8f42145cc81f2cda477
Anuj C More <moreanuj1307@gmail.com>
2021-12-23 22:47:30 +0530

    Banner and linear gradient

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,10 @@ const Home = () => {
   return (
     <div className="home">
       <img
-        src="https://m.media-amazon.com/images/I/51LVRbVTqvL._SX3000_.jpg"
+        // src="https://m.media-amazon.com/images/I/51LVRbVTqvL._SX3000_.jpg" // iphone image
+        src="https://m.media-amazon.com/images/I/61aUfpZteZL._SX3000_.jpg" // Prime image
+        // Actual image
+        // src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
         alt="homeBanner"
         className="home__banner"
       />
