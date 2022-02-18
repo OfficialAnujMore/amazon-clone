7c45c79b184877653ff03d82de20a885bf446b19
Anuj C More <moreanuj1307@gmail.com>
2022-02-18 21:24:55 +0530

    Loader added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,129 +3,12 @@ import "../styles/Home.css";
 import Product from "./Product";
 import colRef from "../dataLayer/firebase-config";
 import { getDocs } from "firebase/firestore";
+import { CircularProgress } from "@mui/material";
 
 const Home = () => {
   const [data, setData] = useState([]);
-  const productData = [
-    {
-      id: "1454646",
-      title:
-        "2020 Apple MacBook Pro (13.3-inch/33.78 cm, Apple M1 chip with 8‑core CPU and 8‑core GPU, 8GB RAM, 512GB SSD) - Space Grey",
-      price: 131990,
-      rating: 4,
-      image: "https://m.media-amazon.com/images/I/71an9eiBxpL._SL1500_.jpg",
-    },
-    {
-      id: "8986234544",
-      title:
-        "Samsung Galaxy M52 5G (Blazing Black, 6GB RAM, 128GB Storage) Latest Snapdragon 778G 5G | sAMOLED 120Hz Display",
-      price: 37999,
-      rating: 2,
-      image: "https://m.media-amazon.com/images/I/71QT7dSK4BL._SL1500_.jpg",
-    },
-    {
-      id: "1113454564",
-      title:
-        "Apple Watch Series 7 (GPS + Cellular, 45mm) - Blue Aluminium Case with Abyss Blue Sport Band - Regular",
-      price: 53900,
-      rating: 3,
-      image: "https://m.media-amazon.com/images/I/71SFsTBSJgL._SL1500_.jpg",
-    },
-    {
-      id: "4654645465",
-      title:
-        "Oneplus Bullets Wireless Z Bass Edition Bluetooth in Ear Earphones with mic (Black)",
-      price: 1999,
-      rating: 2,
-      image: "https://m.media-amazon.com/images/I/510Ns+URsGS._SL1500_.jpg",
-    },
-    {
-      id: "45784132123",
-      title:
-        "Samsung Galaxy M52 5G (Blazing Black, 6GB RAM, 128GB Storage) Latest Snapdragon 778G 5G | sAMOLED 120Hz Display",
-      price: 24999,
-      rating: 2,
-      image: "https://m.media-amazon.com/images/I/71b5BwTCcZL._SL1500_.jpg",
-    },
-    {
-      id: "781213232",
-      title:
-        "Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!",
-      price: 1099.0,
-      rating: 3,
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/51u8ZRDCVoL._SX330_BO1,204,203,200_.jpg",
-    },
-    {
-      id: "87412132121",
-      title:
-        "OnePlus 9RT 5G (Nano Silver, 8GB RAM, 128GB Storage)+Alexa hands-free capable",
-      price: 37999.0,
-      rating: 4,
-      image: "https://m.media-amazon.com/images/I/61NC3RS4RXL._SL1500_.jpg",
-    },
-    {
-      id: "1454646",
-      title:
-        "2020 Apple MacBook Pro (13.3-inch/33.78 cm, Apple M1 chip with 8‑core CPU and 8‑core GPU, 8GB RAM, 512GB SSD) - Space Grey",
-      price: 131990,
-      rating: 4,
-      image: "https://m.media-amazon.com/images/I/71an9eiBxpL._SL1500_.jpg",
-    },
-    {
-      id: "8986234544",
-      title:
-        "Samsung Galaxy M52 5G (Blazing Black, 6GB RAM, 128GB Storage) Latest Snapdragon 778G 5G | sAMOLED 120Hz Display",
-      price: 37999,
-      rating: 3,
-      image: "https://m.media-amazon.com/images/I/71QT7dSK4BL._SL1500_.jpg",
-    },
-    {
-      id: "1113454564",
-      title:
-        "Apple Watch Series 7 (GPS + Cellular, 45mm) - Blue Aluminium Case with Abyss Blue Sport Band - Regular",
-      price: 53900,
-      rating: 3,
-      image: "https://m.media-amazon.com/images/I/71SFsTBSJgL._SL1500_.jpg",
-    },
-    {
-      id: "4654645465",
-      title:
-        "Oneplus Bullets Wireless Z Bass Edition Bluetooth in Ear Earphones with mic (Black)",
-      price: 1999,
-      rating: 2,
-      image: "https://m.media-amazon.com/images/I/510Ns+URsGS._SL1500_.jpg",
-    },
-    {
-      id: "45784132123",
-      title:
-        "Samsung Galaxy M52 5G (Blazing Black, 6GB RAM, 128GB Storage) Latest Snapdragon 778G 5G | sAMOLED 120Hz Display",
-      price: 24999,
-      rating: 2,
-      image: "https://m.media-amazon.com/images/I/71b5BwTCcZL._SL1500_.jpg",
-    },
-    {
-      id: "781213232",
-      title:
-        "Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!",
-      price: 1099.0,
-      rating: 4,
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/51u8ZRDCVoL._SX330_BO1,204,203,200_.jpg",
-    },
-    {
-      id: "87412132121",
-      title:
-        "OnePlus 9RT 5G (Nano Silver, 8GB RAM, 128GB Storage)+Alexa hands-free capable",
-      price: 37999.0,
-      rating: 4,
-      image: "https://m.media-amazon.com/images/I/61NC3RS4RXL._SL1500_.jpg",
-    },
-  ];
 
   useEffect(() => {
-  
-
     getDocs(colRef).then((snapshot) => {
       setData(
         snapshot.docs.map((doc) => ({
@@ -145,7 +28,6 @@ const Home = () => {
     // productData.map((item) => {});
   }, []);
 
-  console.log("asaa666666", data);
   return (
     <div className="home">
       <img
@@ -157,34 +39,26 @@ const Home = () => {
         className="home__banner"
       />
 
-      <div className="home__row">
-        {/* {productData.map((item) => {
-          console.log("data1111", data.length);
-          
-          return (
-            <Product
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          );
-        })} */}
-
-        {data.map((item) => {
-          console.log("newData", item.title);
-          return (
-            <Product
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          );
-        })}
-      </div>
+      {data.length === 0 ? (
+        <div className="loader">
+          <CircularProgress color="warning"  size="4rem"/>
+        </div>
+      ) : (
+        <div className="home__row">
+          {data.map((item) => {
+            console.log("newData", item.title);
+            return (
+              <Product
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
